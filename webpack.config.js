5e81b7faed8fbb89b84db2534568b723e4b5ec96
Georgios Giannoutsos Barkas <contact@nuc.gr>
2017-10-10 12:45:35 +0200

    Add webpack and friends

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  target: 'node',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs',
+    filename: 'index.js',
+  },
+  module: {
+    loaders: [
+      {
+        include: [path.resolve(__dirname, 'src')],
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ]
+  },
+  externals: {
+    'prop-types': 'prop-types',
+    'react': 'react',
+    'react-dom': 'react-dom',
+    'react-helmet': 'react-helmet'
+  },
+  plugins: []
+}

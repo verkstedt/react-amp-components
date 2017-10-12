a75536e50a127a071b46cdbcfc00d074ef12f109
Georgios Giannoutsos Barkas <contact@nuc.gr>
2017-10-12 09:36:11 +0200

    Improve tests

--- src/__tests__/components/Image-test.js
+++ src/__tests__/components/Image-test.js
@@ -28,8 +28,8 @@ describe('Image', () => {
     expect(image.toJSON().props).toMatchObject(props)
   })
 
-  it('renders all the right props', () => {
-    const props = {
+  it('renders only the right props', () => {
+    const validProps = {
       src: 'https://placekitten.com/200/300',
       srcset: '/img/amp.jpg 1080w, /img/amp-900.jpg 900w',
       alt: 'just an alt',
@@ -37,13 +37,23 @@ describe('Image', () => {
       width: 100,
       height: 100,
       className: 'such-a-class',
-      houhou: 'jhoh',
-      randomStuff: 'aiosdo'
+    }
+    const invalidProps = {
+      randomStuff: 'aiosdo',
+      otherStuff: 123
     }
     const image = renderToStaticMarkup(
-      <Image { ...props } />
+      <Image {...{ ...validProps, ...invalidProps } } />
     )
-    expect(image).toMatchSnapshot(props)
+    expect(image).toMatchSnapshot(validProps)
+
+    for (const prop in validProps) {
+      expect(image).toMatch(new RegExp(`${validProps[prop]}`))
+    }
+
+    for (const prop in invalidProps) {
+      expect(image).not.toMatch(new RegExp(`${invalidProps[prop]}`))
+    }
   })
 })
 

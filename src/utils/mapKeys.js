5d99b1a76863e41be0408142554ef9c6aedb6a9d
Matt Lubel <matt_lubel@hotmail.com>
2018-06-13 14:56:07 +0200

    remove mapObj dependency

--- src/utils/mapKeys.js
+++ src/utils/mapKeys.js
@@ -1,5 +1,3 @@
-const mapObj = require('map-obj')
-
 // mapping for some props from camelCase to snake-case
 const map = {
   dataIframeSrc: 'data-iframe-src',
@@ -8,6 +6,7 @@ const map = {
 }
 
 // maps keys according to the above map
-const mapKeys = obj => mapObj(obj, (key, val) => [map[key] || key, val])
+const mapKeys = obj => Object.entries(obj)
+  .reduce((acc, [key, val]) => Object.assign(acc, { [map[key] || key]: val }), {})
 
 export default mapKeys

625c1937648583d9ed2c35f76dd9f00bfe498d4c
Florian <florian@ritzel.me>
2018-11-14 00:13:48 +0100

    I am going to hell for this

--- src/components/AmpDecorator.js
+++ src/components/AmpDecorator.js
@@ -2,11 +2,19 @@ import * as React from 'react'
 
 import Layout from './Layout'
 
-const AmpDecorator = storyFn => (
-  <div>
-    <Layout />
-    { storyFn() }
-  </div>
-)
+const AmpDecorator = (storyFn) => {
+  // TODO: Ok so this is... disgusting. But works for now.
+  // By reloading the location if AMP is defined,
+  // we make HMR and storybook UI navigation work.
+  // I think.
+  if (self.AMP) location.reload()
+
+  return (
+    <div>
+      <Layout />
+      { storyFn() }
+    </div>
+  )
+}
 
 export default AmpDecorator
